2e0b9a889e82c87ba724993470ccdcfcf5b2615b
Robert Koczy <robert.koczy.partner@decathlon.com>
2025-03-14 14:10:02 +0100

    fix: docker-entrypoint.sh start

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,10 +6,8 @@ if [[ -z ${1} ]]; then
   chamber exec "$CHAMBER_SERVICE" -- bundle exec puma -C config/puma.rb
 else
   echo "NOT RUNNING DEFAULT START, USING THE CONTAINER COMMAND INSTEAD: $@"
-  if timeout 10 chamber exec "$CHAMBER_SERVICE" -- "$@"; then
-    exec "$@"
-  else
+  if ! timeout 10 chamber exec "$CHAMBER_SERVICE" -- "$@"; then
     echo "Chamber failed or timed out. Running the command ($@) without secrets."
-    exec "$@"
   fi
+  exec "$@"
 fi
